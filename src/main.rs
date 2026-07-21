5340360a8cd9131f56d38bc37af02220efd6fd79
FaiTheFairy <me@faithefairy.com>
2026-07-21 18:33:34 +0300

    write main game loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::time::Duration;
+
+use anyhow::Context;
+
 use crate::{
     clock::ClockTime,
     render::{AsciiRenderer, ClockRenderer},
@@ -12,10 +16,37 @@ fn main() -> anyhow::Result<()> {
     println!("Terminal size: ({width}x{height})");
 
     let renderer = AsciiRenderer::default();
+    let mut input = String::with_capacity(5);
 
-    let output = renderer.render(ClockTime::new(10, 10, 30), width, height);
+    loop {
+        let time = ClockTime::random();
 
-    println!("{output}");
+        let output = renderer.render(time, width, height - 2);
+        println!("{output}");
+        println!("What's the time? e.g. '12:30'");
+        let now = std::time::Instant::now();
+
+        std::io::stdin().read_line(&mut input)?;
+
+        let (hours, minutes) = input.trim().split_once(':').context("time missing ':'")?;
+        let hours: u8 = hours.parse()?;
+        let minutes = minutes.parse()?;
+
+        if hours == time.hour_12() && ((time.minute() - 5)..(time.minute() + 5)).contains(&minutes)
+        {
+            let elapsed = now.elapsed();
+            println!(
+                "Correct! off by {} minutes",
+                time.minute().abs_diff(minutes)
+            );
+            println!("Answered in {}s", elapsed.as_secs_f32())
+        } else {
+            println!("Incorrect! time is {}:{}", time.hour_12(), time.minute());
+        }
+
+        input.clear();
+        std::thread::sleep(Duration::from_secs(2));
+    }
 
     Ok(())
 }
